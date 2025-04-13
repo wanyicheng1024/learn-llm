0903ca2ac72c10c4fe0cb1653454823dec58a72a
wanyicheng <wanyicheng1024@gmail.com>
2025-04-13 16:10:37 +0800

    添加原书代码和记录遗留的数学知识问题

--- introduction-to-deep-learning1/ch05/train_neuralnet.py
+++ introduction-to-deep-learning1/ch05/train_neuralnet.py
@@ -22,7 +22,7 @@ test_acc_list = []
 
 iter_per_epoch = max(train_size / batch_size, 1)
 
-for i in range(iters_num): 
+for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
@@ -37,10 +37,10 @@ for i in range(iters_num):
     
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
-     
+    
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
         train_acc_list.append(train_acc)
-        test_acc_list.append(test_acc) 
+        test_acc_list.append(test_acc)
         print(train_acc, test_acc)
